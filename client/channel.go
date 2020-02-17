36952435ced573c2e6452a87cdea7e0e73c9ce2b
MariusVanDerWijden <m.vanderwijden@live.de>
2020-02-17 15:28:03 +0100

    :sparkles: [client] Use adjudicator instead of settler

--- client/channel.go
+++ client/channel.go
@@ -8,6 +8,7 @@ package client
 import (
 	"context"
 	"sync"
+	"time"
 
 	"github.com/pkg/errors"
 
@@ -26,11 +27,11 @@ type Channel struct {
 	perunsync.Closer
 	log log.Logger
 
-	conn      *channelConn
-	machine   channel.StateMachine
-	machMtx   sync.RWMutex
-	updateSub chan<- *channel.State
-	settler   channel.Settler
+	conn        *channelConn
+	machine     channel.StateMachine
+	machMtx     sync.RWMutex
+	updateSub   chan<- *channel.State
+	adjudicator channel.Adjudicator
 }
 
 // newChannel is internally used by the Client to create a new channel
@@ -39,7 +40,7 @@ func newChannel(
 	acc wallet.Account,
 	peers []*peer.Peer,
 	params channel.Params,
-	settler channel.Settler,
+	adjudicator channel.Adjudicator,
 ) (*Channel, error) {
 	machine, err := channel.NewStateMachine(acc, params)
 	if err != nil {
@@ -55,10 +56,10 @@ func newChannel(
 	logger := log.WithFields(log.Fields{"channel": params.ID(), "id": acc.Address()})
 	conn.SetLogger(logger)
 	return &Channel{
-		log:     logger,
-		conn:    conn,
-		machine: *machine,
-		settler: settler,
+		log:         logger,
+		conn:        conn,
+		machine:     *machine,
+		adjudicator: adjudicator,
 	}, nil
 }
 
@@ -171,8 +172,27 @@ func (c *Channel) Settle(ctx context.Context) error {
 		return errors.New("currently, only channels in a final state can be settled")
 	}
 
-	if err := c.settler.Settle(ctx, c.machine.SettleReq(), c.machine.Account()); err != nil {
-		return errors.WithMessage(err, "calling settler")
+	req := c.machine.AdjudicatorReq()
+
+	event, err := c.adjudicator.Register(ctx, req)
+	if err != nil {
+		return errors.WithMessage(err, "calling Register")
+	}
+	if event.Version != req.Tx.Version {
+		return errors.Errorf("Invalid version registered want %v, got %v", req.Tx.Version, event.Version)
+	}
+	if event.Timeout.Unix() > time.Now().Unix() {
+		c.log.Warnf("Unexpected withdrawal timeout during Settle(). Waiting until %v", event.Timeout)
+		ticker := time.NewTicker(time.Until(event.Timeout))
+		select {
+		case <-ticker.C:
+		case <-ctx.Done():
+			return errors.New("Canceled during waiting time")
+		}
+	}
+
+	if err := c.adjudicator.Withdraw(ctx, req); err != nil {
+		return errors.WithMessage(err, "calling Withdraw")
 	}
 
 	return c.machine.SetSettled()
